798f4389bafee1776ad863614769e578667d75ff
ghostbuster91 <kghost0@gmail.com>
2020-03-28 22:58:40 +0100

    Fix cache compilation issue

--- cache/src/main/scala/sttp/resilience4s/cache/CacheOps.scala
+++ cache/src/main/scala/sttp/resilience4s/cache/CacheOps.scala
@@ -3,7 +3,7 @@ package sttp.resilience4s.cache
 import io.github.resilience4j.cache.Cache
 import sttp.resilience4s.monad.MonadError
 
-final class CacheOps[F[_], A](action: => F[A]) {
-  def withCache[K, V](cache: Cache[K, V], key: K)(implicit me: MonadError[F]): F[A] =
-    Cache4s.decorateF(cache, action, key)
+final class CacheOps[F[_], V](action: => F[V]) {
+  def withCache[K](cache: Cache[K, V], key: K)(implicit me: MonadError[F]): F[V] =
+    Cache4s.decorateF[K, V, F](cache, action, key)
 }
