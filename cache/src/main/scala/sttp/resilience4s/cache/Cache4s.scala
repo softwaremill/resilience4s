98f84357ff07149eadf3a0395f21521ef71094e4
ghostbuster91 <kghost0@gmail.com>
2020-03-29 14:54:10 +0200

    Cache: cache reflective invocations

--- cache/src/main/scala/sttp/resilience4s/cache/Cache4s.scala
+++ cache/src/main/scala/sttp/resilience4s/cache/Cache4s.scala
@@ -9,22 +9,30 @@ import sttp.resilience4s.monad.syntax._
 object Cache4s {
   def decorateF[K, V, F[_]](cache: Cache[K, V], action: => F[V], key: K)(implicit me: MonadError[F]): F[V] = {
     val cacheImpl = cache.asInstanceOf[CacheImpl[K, V]]
-    new AccessibleCache(cacheImpl).computeIfAbsent(key, action)
+    new CacheAdapter(cacheImpl).computeIfAbsent(key, action)
   }
 }
 
-class AccessibleCache[K, V](cacheImpl: CacheImpl[K, V]) {
+private class CacheAdapter[K, V](cacheImpl: CacheImpl[K, V]) {
 
-  private def putValueIntoCache(key: K, value: V): Unit = {
+  private val _putValueIntoCache = {
     val method = cacheImpl.getClass.getDeclaredMethod("putValueIntoCache")
     method.setAccessible(true)
-    method.invoke(cacheImpl, key, value)
+    method
+  }
+
+  private val _getValueFromCache = {
+    val method = cacheImpl.getClass.getDeclaredMethod("getValueFromCache")
+    method.setAccessible(true)
+    method
+  }
+
+  private def putValueIntoCache(key: K, value: V): Unit = {
+    _putValueIntoCache.invoke(cacheImpl, key, value)
   }
 
   private def getValueFromCache(cacheKey: K): Option[V] = {
-    val method = cacheImpl.getClass.getDeclaredMethod("getValueFromCache")
-    method.setAccessible(true)
-    val vavrOption = method.invoke(cacheImpl, cacheKey).asInstanceOf[control.Option[V]]
+    val vavrOption = _getValueFromCache.invoke(cacheImpl, cacheKey).asInstanceOf[control.Option[V]]
     if (vavrOption.isEmpty) {
       None
     } else {
